33da1cb0cce5d57564586ea3bcbc602801f3cd0b
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-03-24 08:20:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function iterativeLog(array) {
 }
 
 function iterate(callback) {
-  var array = new Array([a, b, c, d, e, f])
+  var array = [a, b, c, d, e, f]
   array.forEach(callback)
   return array
 }
